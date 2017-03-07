410ee889e7c5dcad073ca83ec9e336e1892eb8c0
Noah <noahingham@gmail.com>
2017-03-07 23:20:27 +1100

    Added initial files

---
+++ facerec.py
@@ -0,0 +1,70 @@
+# facerec.py
+import cv2, sys, numpy, os
+
+# Increase (by 1 each time) if running slowly
+size = 1
+
+fn_haar = 'haarcascade_frontalface_default.xml'
+fn_dir = 'att_faces'
+
+# Part 1: Create fisherRecognizer
+print('Training...')
+# Create a list of images and a list of corresponding names
+(images, lables, names, id) = ([], [], {}, 0)
+for (subdirs, dirs, files) in os.walk(fn_dir):
+    for subdir in dirs:
+        names[id] = subdir
+        subjectpath = os.path.join(fn_dir, subdir)
+        for filename in os.listdir(subjectpath):
+            path = subjectpath + '/' + filename
+            lable = id
+            images.append(cv2.imread(path, 0))
+            lables.append(int(lable))
+        id += 1
+(im_width, im_height) = (112, 92)
+
+# Create a Numpy array from the two lists above
+(images, lables) = [numpy.array(lis) for lis in [images, lables]]
+
+# OpenCV trains a model from the images
+# NOTE FOR OpenCV2: remove '.face'
+model = cv2.face.createFisherFaceRecognizer()
+model.train(images, lables)
+
+# Part 2: Use fisherRecognizer on camera stream
+haar_cascade = cv2.CascadeClassifier(fn_haar)
+webcam = cv2.VideoCapture(0)
+while True:
+
+    # Loop untill the camera is working
+    rval = False
+    while(not rval):
+        (rval, frame) = webcam.read()
+        if(not rval):
+            print("Failed to open webcam. Trying again...")
+    frame=cv2.flip(frame,1,0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    mini = cv2.resize(gray, (int(gray.shape[1]/size), int(gray.shape[0]/size)))
+    faces = haar_cascade.detectMultiScale(mini)
+    for i in range(len(faces)):
+        face_i = faces[i]
+        (x, y, w, h) = [v * size for v in face_i]
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (im_width, im_height))
+
+        # Try to recognize the face
+        prediction = model.predict(face_resize)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        # Write the name of recognized face
+        # [1]
+        #cv2.putText(frame,
+        #    '%s' % (names[prediction]),
+        #    (x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+        cv2.putText(frame,
+            '%s - %.0f' % (names[prediction[0]],prediction[1]),
+            (x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+    cv2.imshow('OpenCV', frame)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
